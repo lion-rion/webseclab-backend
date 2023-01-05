13a8f42fcf3669f5cebc4b0cd1a861a12548a233
lion-rion <lionact08@gmail.com>
2023-01-06 02:15:39 +0900

    [chore]

--- src/resources/views/sqli/right-content.blade.php
+++ src/resources/views/sqli/right-content.blade.php
@@ -14,10 +14,36 @@
         <div class="code-toolbar">
             <pre class="line-numbers language-scss" tabindex="0"><code class="language-PHP">&lt;?php echo echo echo echo echo &quot;Hello World&quot; ?&gt;</code></pre>
         </div>
+        <div class="mybox">
+            <p>Username : admin</p>
+            <p>Password : password</p>
+        </div>
+
         <p>あああああ</p>
         <p>あああああ</p>
         <p>あああああ</p>
-        <p>あああああ</p>
+        <!--吹き出しはじまり-->
+        <div class="balloon5">
+            <div class="faceicon">  
+            <img src="/img/target.png" alt="">
+            </div>
+            <div class="chatting">
+            <div class="says">
+                <p>★文章を入れる★</p>
+            </div>
+            </div>
+        </div>
+        <div class="balloon-right">
+            <div class="chatting">
+                <div class="says2">
+                    <p>★文章を入れるるるるるるるるるるるるうるるるるるるるるるるるるうるるるるるるるるるるるるうるるるるる★</p>
+                </div>
+            </div>
+            <div class="faceicon">  
+                <img src="/img/target.png" alt="">
+            </div>
+        </div>
+        <!--吹き出し終わり-->
         <p>あああああ</p>
         <p>あああああ</p>
         <p>あああああ</p>
