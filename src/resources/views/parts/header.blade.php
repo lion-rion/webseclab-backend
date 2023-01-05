5a150ac8c93f13aebf8b0aa0ee01ae218ef30d98
lion-rion <lionact08@gmail.com>
2023-01-06 05:50:37 +0900

    ✅header完成

--- src/resources/views/parts/header.blade.php
+++ src/resources/views/parts/header.blade.php
@@ -1,10 +1,30 @@
 <header>
-    <div class="flex">
-        <div>
-            <img class="main-icon" src="/img/target.png" alt="">
+    <div class="flex justify-between">
+        <div class="flex">
+            <a href="/"><img class="main-icon" src="/img/icon.png" alt=""></a>
+            <div class="global-menu gnavi flex">
+                <li><a href="">コース一覧</a></li>
+                <li><a href="">ぺねらぼについて</a></li>
+                <li><a href="">お問い合わせ</a></li>
+            </div>
         </div>
-        <div>
-            <p>レッスン1</p>
+        <div class="flex ">
+            <div class="global-menu gnavi flex">
+                @guest <!--ログインしていなかったら表示-->
+                <li><a href="{{ url('/login') }}">ログイン</a></li>
+                <li><a href="{{ url('/register') }}">新規登録</a></li>
+                @endguest
+                @auth <!--ログインしていたら表示-->
+                <li><form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('ログアウト') }}
+                    </x-dropdown-link>
+                </form></li>
+                @endauth
+            </div>
         </div>
     </div>
 </header>
