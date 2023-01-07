996b0b71986200bc53f7c7ed8ee2558602154ca7
lion-rion <lionact08@gmail.com>
2023-01-07 12:05:05 +0900

    📝 バリデーションを追加

--- src/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,6 +35,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
+            `checks_id` => ['required'], // I added this line
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
